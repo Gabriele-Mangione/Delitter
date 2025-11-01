37b14e5526f8686a81ee0690e41bf95c75badb23
Andrea Mangione <andrea@mangione.dev>
2025-11-01 10:02:12 +0100

    feat: add firebase

--- code/frontend/svelte.config.js
+++ code/frontend/svelte.config.js
@@ -1,12 +1,19 @@
-import adapter from '@sveltejs/adapter-node';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from "@sveltejs/adapter-static";
+//import { vitePreprocess } from '@sveltejs/kit';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	//	preprocess: vitePreprocess(),
+
+	kit: {
+		adapter: adapter({
+			fallback: "index.html", // Needed for SPA-style routing
+		}),
+
+		// no "ssr" key here anymore!
+		prerender: {
+			entries: [], // disable prerendering
+		},
+	},
 };
 
 export default config;
