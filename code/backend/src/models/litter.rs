0a2839ecc7cf24ff21e0c51f59dc7fa42c616816
Gabriele Mangione <Gabriele.Mangione@hotmail.com>
2025-11-01 14:21:36 +0100

    Litter datastructure and db interface

--- code/backend/src/models/litter.rs
+++ code/backend/src/models/litter.rs
@@ -1,9 +1,15 @@
+use std::time::SystemTime;
+
 use mongodb::bson::oid::ObjectId;
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize, Default)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct Litter {
-    pub _id: Option<ObjectId>,
+    pub _id: ObjectId,
     pub lng: f64,
     pub lat: f64,
+    pub file: Vec<u8>,
+    pub r#type: String,
+    pub tags: Vec<String>,
+    pub time_stamp: mongodb::bson::DateTime,
 }
